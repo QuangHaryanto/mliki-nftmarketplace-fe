9c063f97709660d8a749bca4da05efc01a2f8da7
Julian Purukan <32499848+julianpurukan@users.noreply.github.com>
2022-08-24 16:33:26 +0700

    Update VVConfig.js

--- src/helper/VVConfig.js
+++ src/helper/VVConfig.js
@@ -1,7 +1,7 @@
 export const config = {
     onboard : "78906f42-66e0-42ee-aa59-c29c7f9759f0",
     networkId: 80001,
-    main_address: "0x0bF7eA526D591afb2484373c25E2b538357ae607",
+    main_address: "0x457fc2AE6793e7A5D42A6388EE9383d2aB5f6819",
     currency: "MATIC",
     multi_contract_address: "0x21B8e4e76Cde6Defc9ACf0d494069bd5Ea09f973", //done
     platform_contract_address: "0x62C1fbc83AE2d5CF389b11335FDD4cA4558e1CDC", //done
