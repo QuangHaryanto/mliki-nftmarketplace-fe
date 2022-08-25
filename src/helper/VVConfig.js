c0922e91d706217174c30cca858d031f59cffcba
Julian Purukan <32499848+julianpurukan@users.noreply.github.com>
2022-08-25 19:03:40 +0700

    Update VVConfig.js

--- src/helper/VVConfig.js
+++ src/helper/VVConfig.js
@@ -1,12 +1,12 @@
 export const config = {
     onboard : "78906f42-66e0-42ee-aa59-c29c7f9759f0",
-    networkId: 80001,
+    networkId: 137,
     main_address: "0x457fc2AE6793e7A5D42A6388EE9383d2aB5f6819",
     currency: "MATIC",
     multi_contract_address: "0x21B8e4e76Cde6Defc9ACf0d494069bd5Ea09f973", //done
     platform_contract_address: "0x62C1fbc83AE2d5CF389b11335FDD4cA4558e1CDC", //done
-    explorer: "https://mumbai.polygonscan.com/",
-    rpc_url: "https://matic-mumbai.chainstacklabs.com",
+    explorer: "https://polygonscan.com/",
+    rpc_url: "https://polygon-rpc.com/",
     contract_address: "0x934EB0dB4f9cC538FFB7C6279F204818C85fbfCa", //done
     block_chain: "Polygon"
 }
