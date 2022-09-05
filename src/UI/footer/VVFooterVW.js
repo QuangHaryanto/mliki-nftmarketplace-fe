97f7aa243952cb0e456e1c3992b6f52aff4914e1
Julian Purukan <32499848+julianpurukan@users.noreply.github.com>
2022-09-05 08:28:31 +0700

    Update VVFooterVW.js

--- src/UI/footer/VVFooterVW.js
+++ src/UI/footer/VVFooterVW.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { getOptions } from '../../services/VVUserService';
+import axios from "axios";
+import { getItemListExplorerAPI } from '../../services/VVItemService';
+import VVItemModal from '../../models/VVItemModal';
+import {listAPI} from './../../services/VVCategoryService';
+import { allCollectionsAPI } from '../../services/VVCollectionService';
+import VVAssetVW from '../../UI/asset/VVAssetVW';
+import VVnodataVW from '../../UI/nodata/VVnodataVW'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const scrollToTop = () =>{
 	window.scrollTo({
@@ -12,7 +20,27 @@ const scrollToTop = () =>{
 class VVFooterVW extends React.Component {
   constructor() {
     super()
+	this.page = 1;
+    this.loading = false;
+    this.user = null;
+    this.firstTime = false
+    this.collectionId = "";
+    this.categoryId= "";
+    this.sortby="latest";
+    this.keyword = ""
+    this.cancelTokenSource = null;
     this.state = {
+			skeleton: [1,1,1,1,1,1,1,1,1,1,1,1],
+			items: [],
+			paging: false,
+			loading: true,
+			collection_id: "",
+			category_id: "",
+			sortby: this.sortby,
+			keyword: '',
+			categories:[],
+			collections:[],
+			marketplace_title: '',
 			facebook:'',
 			messenger:'',
 			whatsapp:'',
@@ -25,6 +53,14 @@ class VVFooterVW extends React.Component {
 
   componentDidMount() {
     this.getFooter(); 
+	this.setState({
+		items: [],
+		loading: true,
+		paging: false,
+	  });
+	  this.page = 1;
+	  this.getCollections();
+	  this.getStaticcontent()
   }
 
   getFooter = () => {
@@ -44,6 +80,126 @@ class VVFooterVW extends React.Component {
     })
   }
 
+  getStaticcontent = () => {
+    getOptions('title').then(result=>{
+      if(result.status === true) {
+        var parse = JSON.parse(result.result.value);
+        console.log('parse ->', parse)
+        this.setState({
+          marketplace_title: parse.marketplace_page_title
+        })
+      }
+    })
+  }
+
+  itemAPIAction = (keyword,page) => {
+    var params = {
+      type: "explorer",
+      keyword: keyword,
+      sortby:this.sortby,
+      collection_id: this.collectionId,
+      page:page
+    };
+
+    if(this.categoryId.trim().length>0) {
+        params["category_id"] = this.categoryId
+    }
+
+    if(this.collectionId.trim().length>0) {
+      params["collection_id"] = this.collectionId
+    }
+    this.cancelTokenSource = axios.CancelToken.source();
+    getItemListExplorerAPI(params,this.cancelTokenSource).then(result=>{
+      if(result.status === true) {
+        this.cancelTokenSource = null;
+        var tempArray = VVItemModal.parseItemList(result.data.docs);
+        var items = this.state.items
+        if(page !== 1) {
+           for (let index = 0; index < tempArray.length; index++) {
+            items.push(tempArray[index])
+           }
+        } else {
+            items = tempArray 
+        }
+        this.setState({
+          loading: false,
+          paging: result.data.docs.length>0 ? true : false,
+          items: items
+        })
+      }
+    })
+  }
+
+  fetchMoreData = () => {
+    this.page = this.page + 1;
+    this.itemAPIAction(this.keyword,this.page)
+  }
+  changeKeyword = (e) => {
+    this.page = 1;
+    let search_text = e.target.value;
+    this.keyword = search_text;
+    if(this.cancelTokenSource) {
+      this.cancelTokenSource.cancel();
+    }
+    this.setState({
+      items: [],
+      loading: true,
+      paging: false,
+      keyword: e.target.value,
+    });
+    this.itemAPIAction(this.keyword,this.page)
+  }
+  changeType = (e) => {
+    this.page = 1;
+    this.sortby = e;
+    this.setState({
+      items: [],
+      loading: true,
+      paging: false,
+      sortby: this.sortby,
+    });
+    this.itemAPIAction(this.keyword,this.page)
+  }
+  changeCategory = (category_id) => {
+    this.page = 1;
+    this.categoryId = category_id;
+    this.setState({
+      items: [],
+      loading: true,
+      paging: false,
+      category_id: this.categoryId,
+    });
+    this.itemAPIAction(this.keyword,this.page)
+  }
+  changeCollection = (e) => {
+    this.page = 1;
+    this.collectionId = e.target.value;
+    this.setState({
+      items: [],
+      loading: true,
+      paging: false,
+      collection_id: this.collectionId,
+    });
+    this.itemAPIAction(this.keyword,this.page)
+  }
+  getCategories = () => {
+    listAPI().then(result=>{
+      this.setState({
+        categories:result.data
+		
+      });
+	  console.log('dara',result)
+      this.itemAPIAction(this.keyword,this.page)
+    })
+  }
+  getCollections = () => {
+    allCollectionsAPI().then(result=>{
+      this.setState({
+        collections:result.data
+      });
+      this.getCategories()
+    })
+  }
 
   render() {
     return (
@@ -89,9 +245,47 @@ class VVFooterVW extends React.Component {
 			</>
 			}
 		</div>
-	  { (!this.state.footerloading) &&
-	  	<div className="col-lg-6 space-y-20" dangerouslySetInnerHTML={{__html: this.state.footer_html}} />
-		}
+		<div class="col-lg-2 col-6">
+			<h6 class="footer__title">Marketplace</h6>
+			<ul class="footer__list">
+			<li>
+			<Link to="/marketplace" className={"" + (this.state.path == "/" ? 'color_brand' : 'color_white')}>All NFTs</Link>
+          </li>
+			{this.state.categories?.map((item, index) => (
+				
+				<li>
+					<Link to="/marketplace" className={"" + (this.state.path == "/" ? 'color_brand' : 'color_white')} onClick={()=>{
+					this.changeCategory(item._id)
+				}}>{item.title}</Link>
+				</li>
+			))}
+		  </ul>
+		</div>
+		<div class="col-lg-2 col-6">
+			<h6 class="footer__title">Assets</h6>
+			<ul class="footer__list">
+			<li> <Link to="/" className={"" + (this.state.path == "/" ? 'color_brand' : 'color_white')}>Home</Link>
+				</li>
+				<li> <Link to="/creators" className={"" + (this.state.path == "/creators" ? 'color_brand' : 'color_white')}>Creators</Link>
+				</li>
+				<li> <Link to="/marketplace" className={"" + (this.state.path == "/marketplace" ? 'color_brand' : 'color_white')}>Marketplace</Link>
+				</li>
+				<li> <Link to="/activity" className={"" + (this.state.path == "/activity" ? 'color_brand' : 'color_white')}>Activity</Link>
+				</li>
+			</ul>
+		</div>
+		<div class="col-lg-2 col-6">
+			<h6 class="footer__title">Company</h6>
+			<ul class="footer__list">
+				<li> <a href="https://www.mliki.com/" target="_blank" > About </a>
+				</li>
+				<li> <a href="https://mliki-1.gitbook.io/mliki-nft-marketplace-docs/faq" target="_blank" > FAQ </a>
+				</li>
+				<li> <a href="https://mliki-1.gitbook.io/mliki-nft-marketplace-docs/term-of-service" target="_blank"> Term Of Service </a> </li>
+				<li> <a href="https://mliki-1.gitbook.io/mliki-nft-marketplace-docs/privacy-policy" target="_blank"> Privacy Policy
+					</a> </li>
+			</ul>
+		</div>
 	</div>
 	{ (!this.state.footerloading) &&
 	<p className="copyright text-center">
