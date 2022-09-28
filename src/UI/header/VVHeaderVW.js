b4a9693500002dcd847ce3efa229a11c7448f6ad
Julian Purukan <32499848+julianpurukan@users.noreply.github.com>
2022-09-28 08:52:00 +0700

    Update VVHeaderVW.js

--- src/UI/header/VVHeaderVW.js
+++ src/UI/header/VVHeaderVW.js
@@ -1144,17 +1144,26 @@ changeNetwork = async ({ networkName, setError }) => {
 			}
 	}
 
-	loginUser = (params) => {
-		loginAPI(params).then(result=>{
+	loginUser = async(params) => {
+		if ( window.ethereum.networkVersion == this.props.config.networkId ){
+			loginAPI(params).then(result=>{
 			if(result.status == true) {
 				localStorage.setItem("token",result.token);
 				this.setState({
-					isLoggedIn : true
+				isLoggedIn : true
 				})
 				this.configureUser();
 				
 			}
-		})
+			})
+		}else{
+			localStorage.removeItem("wallet");
+			localStorage.removeItem("token");
+			this.setState({
+				isLoggedIn : false
+			})
+			this.props.history.push("/"); 
+		}	
 	}
 
 	configureUser = async() => {
