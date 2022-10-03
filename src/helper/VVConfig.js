fcc3be888d4a2cb30ca8d3c9aff6d99eb54a98d4
Julian Purukan <32499848+julianpurukan@users.noreply.github.com>
2022-10-03 18:45:55 +0700

    Update VVConfig.js

--- src/helper/VVConfig.js
+++ src/helper/VVConfig.js
@@ -8,7 +8,7 @@ export const networkConfig = {
     platform_contract_address: '0xF1cfA37d496a2252D78B5D330CAB55E4E330db6A', // Auction_Polygon
     explorer: 'https://mumbai.polygonscan.com/',
     rpc_url: 'https://matic-mumbai.chainstacklabs.com',
-    contract_address: '0x6B984f9DEc5146e0A675Ee42Fd6588a826F1EE6c', // ERC_Polygon
+    contract_address: '0xe74599520DFdD48D0179628ef07eAd61698bF658', // ERC_Polygon
     block_chain: 'POLYGON'
   },
   OKEX: {
@@ -20,7 +20,7 @@ export const networkConfig = {
     platform_contract_address: '0x021863dD8b0285Ad0CEf0DD4Fb3738FbD71b3F6E', // Auction_OKX
     explorer: 'https://www.oklink.com/okc-test/',
     rpc_url: 'https://exchaintestrpc.okex.org',
-    contract_address: '0x8C5d5C062fe88Cb5E5306E2EB7A8BCA11F752F84', // ERC_OKX
+    contract_address: '0x1BBfD5f57c02b1cCb28bdC71BEd4C8efA8afEEE1', // ERC_OKX
     block_chain: 'OKEX'
   }
 }
